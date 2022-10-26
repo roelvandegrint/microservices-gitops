7f570b22d6cb37a36aa1848032acff6f015200a3
Roel van de Grint <roelvandegrint@gmail.com>
2022-10-26 16:43:26 +0200

    Add backend api

--- client-facing/Controllers/HostNameController.cs
+++ client-facing/Controllers/HostNameController.cs
@@ -1,4 +1,6 @@
-using System.Net;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MultiArchApi.Controllers;
@@ -7,9 +9,49 @@ namespace MultiArchApi.Controllers;
 [Route("[controller]")]
 public class HostNameController : ControllerBase
 {
-    [HttpGet]
-    public string Get()
+    private readonly IConfiguration _config;
+    // private readonly HttpClient httpClient;
+    // private readonly IHostnameService hostnameService;
+
+    public HostNameController(IConfiguration config
+    // , HttpClient httpClient, IHostnameService hostnameService
+    )
     {
-        return Dns.GetHostName();
+        _config = config;
+        // this.httpClient = httpClient;
+        // this.hostnameService = hostnameService;
+    }
+
+    [HttpGet]
+    public async Task<string> Get()
+    {
+        var baseUriString = _config.GetValue<string>("BackendApiBaseAddress");
+        var baseUri = new Uri(baseUriString);
+        var client = new HttpClient
+        {
+            BaseAddress = baseUri
+        };
+
+        return await client.GetStringAsync("hostname");
+    }
+}
+
+public interface IHostnameService
+{
+    Task<string> GetHostName();
+}
+
+public class HostNameService : IHostnameService
+{
+    private readonly HttpClient httpClient;
+
+    public HostNameService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public Task<string> GetHostName()
+    {
+        return httpClient.GetStringAsync("hostname");
     }
 }
--- client-facing/Program.cs
+++ client-facing/Program.cs
@@ -1,3 +1,5 @@
+using MultiArchApi.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IHostnameService, HostNameService>();
+var baseUri = builder.Configuration.GetValue<string>("BackendApiBaseAddress");
+builder.Services.AddHttpClient<HostNameService>(options =>
+{
+    options.BaseAddress = new Uri(baseUri);
+});
+builder.Services.AddHttpClient<HostNameController>(options =>
+{
+    options.BaseAddress = new Uri(baseUri);
+});
 
 var app = builder.Build();
 
