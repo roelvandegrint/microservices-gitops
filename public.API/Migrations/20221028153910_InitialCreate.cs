8266e8911404abb6304dd93f21368933e5f54332
Roel van de Grint <roelvandegrint@gmail.com>
2022-10-28 15:47:41 +0000

    Updated public API to use GUID as ID column

--- public.API/Migrations/20221028153910_InitialCreate.cs
+++ public.API/Migrations/20221028153910_InitialCreate.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -12,8 +13,7 @@ namespace Public.API.Migrations
                 name: "Employees",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
                     FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Prefix = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     LastName = table.Column<string>(type: "nvarchar(max)", nullable: false)
--- public.API/Models/Employee.cs
+++ public.API/Models/Employee.cs
@@ -2,7 +2,7 @@ namespace Public.Api.Models;
 
 public class Employee
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string FirstName { get; set; } = null!;
     public string? Prefix { get; set; }
     public string LastName { get; set; } = null!;
